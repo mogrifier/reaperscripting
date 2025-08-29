90959200441381f20e9554e90b3755b8351f00ba
Erich Izdepski <mogrifier@gmail.com>
2025-08-29 17:16:03 -0400

    now sets all solo/mute off before setting any on

--- trackcontrol.lua
+++ trackcontrol.lua
@@ -1,9 +1,21 @@
 -- trackcontrol.lua
 -- Solo and mute specific tracks in REAPER
 
+--reset all tracks to unsoloed and unmuted
+local num_tracks = reaper.CountTracks(0)
+for i = 1, num_tracks - 1 do
+  local track = reaper.GetTrack(0, i)
+  if track then
+    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", 0)  -- unsolo track
+    reaper.SetMediaTrackInfo_Value(track, "B_MUTE", 0)  -- unmute track
+  end
+end
+reaper.UpdateArrange()
+
+--now based on the midi cc message received, solo or mute specific tracks
 -- Specify track numbers to solo and mute (1-based index but reaper is 0-based. fixed later with a -1
-local tracks_to_solo = {1, 2} -- Example: solo tracks 1 and 2
-local tracks_to_mute = {3, 4} -- Example: mute tracks 3 and 4
+local tracks_to_solo = {5} -- Example: solo tracks 1 and 2
+local tracks_to_mute = {2} -- Example: mute tracks 3 and 4
 
 -- Get the number of tracks in the project
 local num_tracks = reaper.CountTracks(0)
